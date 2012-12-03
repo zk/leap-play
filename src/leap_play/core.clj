25e6ecd4d18d14a346767afc5b3def88b79f4005
zkim <zachary.kim@gmail.com>
2012-12-02 18:31:46 -0800

    initial

---
+++ src/leap_play/core.clj
@@ -0,0 +1,6 @@
+(ns leap-play.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/leap_play/core_test.clj
@@ -0,0 +1,7 @@
+(ns leap-play.core-test
+  (:use clojure.test
+        leap-play.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
