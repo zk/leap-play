e29b5f6130e07ef3d42efa5cf4a87a2437d64ad9
Adam LaFave <lafave.adam@gmail.com>
2013-01-12 18:42:02 -0800

    bump leapmotion sdk version

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.leapmotion/leapsdk "0.1.0"]
+                 [com.leapmotion/leapsdk "0.7.1"]
                  [quil "1.6.0"]]
   :native-path "native")
